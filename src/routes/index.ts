393af3549527d1af8d7009e38038285f15898cbf
Jeremiah Akpera <jerryakpera@gmail.com>
2023-04-02 19:10:02 +0100

    Code clean up for /health endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
+router.get('/health', async (req: Request, res: Response) => {
+  return res.send('UP');
+});
+
 router.use('/', (req: Request, res: Response) => {
   return res.send('Routes');
 });
